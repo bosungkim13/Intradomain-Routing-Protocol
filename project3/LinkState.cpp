445e7a07daa212ad39d2406beca289f00ec1b3ae
Bo Sung Kim <bosungkim@Bos-MacBook-Air.attlocal.net>
2024-11-08 00:06:50 -0600

    djikstras. implement update of table using said djikstra.

--- project3/LinkState.cpp
+++ project3/LinkState.cpp
@@ -1,5 +1,6 @@
 #include "LinkState.h"
 #include <cassert>
+#include <unordered_set>
 
 LinkState::LinkState(Node* n, router_id id, adjacencyList_ref adjList, portStatus_ref portStatus, forwardingTable_ref forwardingTable, port_num numPorts) 
     : sys(n), myRouterID(id), adjacencyList(adjList), portStatus(portStatus), forwardingTable(forwardingTable), numPorts(numPorts), seqNum(0) {
@@ -60,5 +61,86 @@ void LinkState::FloodUpdates(port_num myPort, void* floodMe, unsigned short size
         sys->send(portId, copy, size);
     }
     free(floodMe);
+}
+
+void LinkState::UpdateTable() {
+    // destination router : <current router ID, cost>
+    unordered_map<router_id, std::pair<router_id, cost>> activeDistances;
+
+    // adjacent routers to current router
+    for (auto entry = portStatus.begin(); entry != portStatus.end(); entry++) {
+        PortStatusEntry pEntry = entry->second;
+        if (pEntry.isUp) {
+            activeDistances[pEntry.destRouterID] = std::make_pair(this->myRouterID, pEntry.timeCost);
+        }
+        
+    }
+
+    unordered_set<router_id> toVisit;
+    // all other routers
+    for (auto entry = costTable.begin(); entry != costTable.end(); entry++) {
+        router_id destId = entry->first;
+        if (destId != this->myRouterID) {
+            toVisit.insert(destId);
+            if (activeDistances.find(destId) == activeDistances.end()) {
+                // this destination is not connected to the current router
+                activeDistances[destId] = make_pair(this->myRouterID, INFINITY_COST);
+            }
+        }
+    }
+
+    // DIJKSTRA
+    router_id currRouterId;
+    cost minCost;
+
+    while (!toVisit.empty()) {
+        minCost = INFINITY_COST;
+        for (auto id: toVisit) {
+            if (activeDistances[id].second < minCost) {
+                currRouterId = id;
+                minCost = activeDistances[id].second;
+            }
+        }
+
+        if (minCost == INFINITY_COST) {
+            // There are no paths to current root router
+            break;
+        }
+        toVisit.erase(currRouterId);
+
+        auto currRouterCost = costTable[currRouterId];
+        for (auto id: currRouterCost) {
+            router_id neighborId = id.first;
+            cost neighborCost = id.second;
+            if (toVisit.find(neighborId) != toVisit.end() && neighborCost + minCost < activeDistances[neighborId].second) {
+                activeDistances[neighborId] = make_pair(currRouterId, neighborCost + minCost);
+            }
+        }
+
+
+    }
+
+    // Update the forwarding table and all the info in nodeTable
+    // Update distances for neighbors
+    ls_path_info pathInfo = nodeTable[currRouterId];
+    for (auto distance : activeDistances) {
+        router_id destId = distance.first;
+        pair<router_id, cost> oldDistance = distance.second;
+        if (distance.first != currRouterId) {
+            ls_path_info newPathInfo(destId, sys->time(), oldDistance.second);
+            nodeTable[destId] = newPathInfo;
+            // update forwarding table if cheaper path is found
+            forwardingTable[destId] = this->FindNextHop(activeDistances, destId);
+        }
+    }
+}
+
+router_id LinkState::FindNextHop(unordered_map<router_id, std::pair<router_id, cost>> activeDistances, router_id destId) {
+    router_id currDest = destId;
+    // start at destination and find where do I need to hop next?
+    while (activeDistances[currDest].first != this->myRouterID) {
+        currDest = activeDistances[currDest].first;
+    }
+    return currDest;
 }
 
--- project3/LinkState.h
+++ project3/LinkState.h
@@ -13,9 +13,12 @@ class LinkState {
         void SendUpdates();
         // Function that forwards updates to all neighbors. This function is called when an update is received from a neighbor.
         void FloodUpdates(port_num myPort, void* floodMe, unsigned short size);
-
         // Function that creates a LS packet
         Packet CreatePacket(unsigned short size);
+        // Function to handle LS packets
+        void HandlePacket(port_num portId, void* packet, unsigned short size);
+        // Function to find the next hop for a given destination
+        router_id FindNextHop(unordered_map<router_id, std::pair<router_id, cost>> activeDistances, router_id destId);
 
         LinkState(Node* n, router_id id, adjacencyList_ref adjList, portStatus_ref portStatus, forwardingTable_ref forwardingTable, port_num numPorts);
         ~LinkState();
@@ -24,6 +27,15 @@ class LinkState {
         adjacencyList_ref adjacencyList;
         portStatus_ref portStatus;
         forwardingTable_ref forwardingTable;
+
+        // map from router to previous router and cost
+        unordered_map<router_id, ls_path_info> nodeTable;
+        // map from router to next router and cost. to be updated by the pong handling
+        unordered_map<router_id, unordered_map<router_id, cost>> costTable;
+        // map from router to sequence number
+        unordered_map<router_id, seq_num> seqTable;
+
+
         Node* sys;
         port_num numPorts;
         unsigned int seqNum = 0;
